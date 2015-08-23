38ea456cd46d551e1454282786a5c808d91fdb24
Kevin <ksaldana1@yahoo.com>
2015-08-23 16:21:50 -0500

    add CheckNums

---
+++ Coderbyte-Challenges/8.CheckNums.js
@@ -0,0 +1,38 @@
+function CheckNums(num1,num2) { 
+	if (num2 === num1) {
+		return -1;
+	}
+	else if (num2 > num1) {
+		return true;
+	}
+	else {
+		return false;
+	}
+
+  // code goes here  
+
+         
+}
+   
+// keep this function call here 
+// to see how to enter arguments in JavaScript scroll down
+CheckNums(readline());                            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                            
+                            
+                            
+  
