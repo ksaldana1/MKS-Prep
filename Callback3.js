15cb5b4d47ea41588aa21cc73f62b599a9745c0f
Kevin <ksaldana1@yahoo.com>
2015-08-23 13:25:51 -0500

    Update and rename Callback3.html to Callback3.js

--- Callback3.js
+++ Callback3.js
@@ -1,4 +1,4 @@
-<script>
+
 var arr1 = [1, 2, 3]
 // Callback Functions
 function add1(value) {
@@ -11,4 +11,4 @@ function times2(value) {
 // Invoke with Host Functions
 arr1.map(add1); // returns [2, 3, 4]
 arr1.map(times2) // returns [2, 4, 6]
-</script>
+
