91bb3d0c39bb3e912c011280eadfb6bb2fbd2a70
Kevin <ksaldana1@yahoo.com>
2015-08-23 13:26:32 -0500

    Update and rename lengthofstring.html to lengthofstring.js

--- lengthofstring.js
+++ lengthofstring.js
@@ -1,7 +1,6 @@
-<script>
+
 function lengthofString(string) {
 	var stringArray = string.split(" ");
 	var stringLength = string.split(" ").join("");
 	console.log("This string has " + stringArray.length + " words. The average length of each word is " + (stringLength.length / stringArray.length) + " characters");
 };
-</script>
